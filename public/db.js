bbbc0ffd04bd2b729ffcc056d3ddd3d9171e54b4
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-30 10:32:42 -0700

    Finished checkDatabase function. Added function to check database for data as well as adding a listener for when the app comes back online then calls the checkDatabase function

--- public/db.js
+++ public/db.js
@@ -25,4 +25,37 @@ request.onerror = ({ event }) => {
 function saveRecord(records) {
     const transactions = budgetDb.transactions(['newData', 'readwrite']);
     const store = transactions.objectStore('newData');
-}
+    store.add(records);
+};
+
+//Checking database for data
+function checkDatabase() {
+    const transactions = budgetDb.transactions(['newData', 'readwrite']);
+    const store = transactions.objectStore('newData');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then(response => {
+                    return response.json();
+                })
+                .then((data) => {
+                    const transactions = budgetDb.transactions(['newData'], 'readwrite');
+                    const store = transactions.objectStore('newData');
+                    store.clear();
+                });
+        }
+    };
+};
+
+//adding a listener to listen for when the app comes back online
+window.addEventListener('online', checkDatabase);
+
