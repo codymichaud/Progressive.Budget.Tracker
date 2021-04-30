a5eaf650c5be583b3ca30533d52e694144451621
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-30 09:40:46 -0700

    Finished function for checking online/offline status

--- public/db.js
+++ public/db.js
@@ -1,9 +1,16 @@
 const { response } = require("express");
 
 let budgetDb;
-
 const request = indexedDB.open('budget', 1);
-request.onupgradeneeded = function (event) {
+
+request.onupgradeneeded = ({ event }) => {
     let budgetDb = event.target.result;
     budgetDb.createObject('newData', { autoIncrement });
+};
+
+request.onsuccess = ({ event }) => {
+    budgetDb = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
 };
