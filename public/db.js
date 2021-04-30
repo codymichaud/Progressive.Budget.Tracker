ee73495ce3c54ea580f1206cd91c632ace0fcab3
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-30 09:50:55 -0700

    Added code that will display that there is an error with the error code

--- public/db.js
+++ public/db.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 
-let budgetDb;
+let budgetDb; //creating database
 const request = indexedDB.open('budget', 1);
 
 request.onupgradeneeded = ({ event }) => {
@@ -8,9 +8,16 @@ request.onupgradeneeded = ({ event }) => {
     budgetDb.createObject('newData', { autoIncrement });
 };
 
+//Checks online and offline status
 request.onsuccess = ({ event }) => {
     budgetDb = event.target.result;
     if (navigator.onLine) {
         checkDatabase();
     }
 };
+
+//if an error is present will display error in console with error code
+request.onerror = ({ event }) => {
+    console.log('Looks like there an error ' + event.target.errorCode);
+};
+
